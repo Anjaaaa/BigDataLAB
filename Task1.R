befddec135a69ac9e73108dab800aed5336a88b1
Anja Beck <xanjita@gmx.de>
2017-11-23 23:15:29 +0100

    Task 1.3

--- Task1.R
+++ Task1.R
@@ -26,8 +26,8 @@ dev.off()
 
 ########################################################
 # 1.2 Linear Discriminant Analysis
-diagnosis_lda <- lda(Data$Diagnosis ~ PC_1 + PC_2, data= Data)
-diagnosis_lda2 <- lda(Data$Diagnosis ~ PC_1 + PC_2, data= Data, CV=TRUE)
+diagnosis_lda <- lda(Data$Diagnosis ~ PC_1 + PC_2, data=Data)
+diagnosis_lda2 <- lda(Data$Diagnosis ~ PC_1 + PC_2, data=Data, CV=TRUE)
 
 # classifier: Maybe we don't need this, becaus the predict
 # function can be used with diagnosis_lda...
@@ -48,3 +48,48 @@ diagnosis_lda2 <- lda(Data$Diagnosis ~ PC_1 + PC_2, data= Data, CV=TRUE)
 table(diagnosis_lda2$class, Data[,1])
 prediction <- predict(diagnosis_lda, Data)$class
 
+# calculates the correctly classified patients
+correct = 0
+for (i in 1:length(as.data.frame(t(Data)))){
+  if (prediction[i] == Data$Diagnosis[i]){
+    correct = correct + 1
+  }
+}
+
+
+########################################################
+# 1.3 Logistic Regression
+# get binary data
+diagnosis_bin <- array(0, dim=c(length(Data$Diagnosis),1))
+for (i in 1:length(diagnosis_bin)){
+  if (Data$Diagnosis[i] == "B"){
+    diagnosis_bin[i] = 0
+  } else {
+    diagnosis_bin[i] = 1
+  }
+}
+# actual regression
+diagnosis_lr <- glm(diagnosis_bin ~ PC_1+PC_2, family=binomial, data=Data)
+summary(diagnosis_lr) # display results
+
+# classifier
+coeff <- diagnosis_lr$coefficients
+prob_M = 1 / (1 + exp(-(coeff[1] + PC_1*coeff[2] + PC_2*coeff[3])))
+pred_M <- array(0, dim=c(1,length(prob_M)))
+for (i in 1:length(prob_M))
+if (prob_M[i] > .5){
+  pred_M[i] = 'M'
+} else {
+  pred_M[i] ='B'
+}
+
+# calculates the correctly classified patients
+correct = 0
+for (i in 1:length(as.data.frame(t(Data)))){
+  if (pred_M[i] == Data$Diagnosis[i]){
+    correct = correct + 1
+  }
+}
+
+########################################################
+# 1.4 K-Nearest Neighbor
