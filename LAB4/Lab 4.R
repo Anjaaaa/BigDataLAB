bce9b07107910f4828ba15a3e215b35d41b09013
Anja Beck <xanjita@gmx.de>
2018-01-18 13:03:08 +0100

    silhouette plots hinzugefuegt

--- LAB4/Lab 4.R
+++ LAB4/Lab 4.R
@@ -59,121 +59,67 @@ plot(hclust(data.dist), labels=nci.labs, main="Complete Linkage", xlab="", sub="
 plot(hclust(data.dist, method="average"), labels=nci.labs, main="Average Linkage", xlab="", sub="", ylab="")
 plot(hclust(data.dist, method="single"), labels=nci.labs, main="Single Linkage", xlab="", sub="", ylab="")
 
-hc.out=hclust(dist(sd.data))
 
 hc.out=hclust(dist(sd.data))
 hc.clusters=cutree(hc.out,4)
-table(hc.clusters,nci.labs)
+print(table(nci.labs,hc.clusters))
 
-par(mfrow=c(1,1))
-plot(hc.out, labels=nci.labs)
+hc.out=hclust(dist(sd.data), method="average")
+hc.clusters=cutree(hc.out,4)
+print(table(nci.labs,hc.clusters))
+
+hc.out=hclust(dist(sd.data), method="single")
+hc.clusters=cutree(hc.out,4)
+print(table(nci.labs,hc.clusters))
+
+par(mfrow=c(1,3))
+set.seed(2)
+plot(hclust(data.dist), labels=nci.labs, main="Complete Linkage", xlab="", sub="",ylab="")
 abline(h=139, col="red")
+set.seed(2)
+plot(hclust(data.dist, method="average"), labels=nci.labs, main="Average Linkage", xlab="", sub="", ylab="")
+abline(h=123, col="red")
+set.seed(2)
+plot(hclust(data.dist, method="single"), labels=nci.labs, main="Single Linkage", xlab="", sub="", ylab="")
+abline(h=110, col="red")
 
 ################################################
 #
 # Task 2b K-means clustering
 #
 ################################################
-set.seed(2)
-km.out2=kmeans(sd.data, 2, nstart=20)
-km.out2$betweenss/km.out2$totss
-km.clusters2=km.out2$cluster
-table(km.clusters2,hc.clusters)
-
-set.seed(2)
-km.out3=kmeans(sd.data, 3, nstart=20)
-km.out3$betweenss/km.out3$totss
-km.clusters3=km.out3$cluster
-table(km.clusters3,hc.clusters)
-
-set.seed(2)
-km.out4=kmeans(sd.data, 4, nstart=20)
-km.out4$betweenss/km.out4$totss
-km.clusters4=km.out4$cluster
-table(km.clusters4,hc.clusters)
-
-set.seed(2)
-km.out5=kmeans(sd.data, 5, nstart=20)
-km.out5$betweenss/km.out5$totss
-km.clusters5=km.out5$cluster
-table(km.clusters5,hc.clusters)
-
-set.seed(2)
-km.out6=kmeans(sd.data, 6, nstart=20)
-km.out6$betweenss/km.out6$totss
-km.clusters6=km.out6$cluster
-table(km.clusters6,hc.clusters)
-
-set.seed(2)
-km.out7=kmeans(sd.data, 7, nstart=20)
-km.out7$betweenss/km.out7$totss
-km.clusters7=km.out7$cluster
-table(km.clusters7,hc.clusters)
-
-set.seed(2)
-km.out8=kmeans(sd.data, 8, nstart=20)
-km.out8$betweenss/km.out8$totss
-km.clusters8=km.out8$cluster
-table(km.clusters8,hc.clusters)
-
-set.seed(2)
-km.out9=kmeans(sd.data, 9, nstart=20)
-km.out9$betweenss/km.out9$totss
-km.clusters9=km.out9$cluster
-table(km.clusters9,hc.clusters)
-
-set.seed(2)
-km.out10=kmeans(sd.data, 10, nstart=20)
-km.out10$betweenss/km.out10$totss
-km.clusters10=km.out10$cluster
-table(km.clusters10,hc.clusters)
-
-set.seed(2)
-km.out11=kmeans(sd.data, 11, nstart=20)
-km.out11$betweenss/km.out11$totss
-km.clusters11=km.out11$cluster
-table(km.clusters11,hc.clusters)
-
-set.seed(2)
-km.out12=kmeans(sd.data, 12, nstart=20)
-km.out12$betweenss/km.out12$totss
-km.clusters12=km.out12$cluster
-table(km.clusters12,hc.clusters)
-
-set.seed(2)
-km.out13=kmeans(sd.data, 13, nstart=20)
-km.out13$betweenss/km.out13$totss
-km.clusters13=km.out13$cluster
-table(km.clusters13,hc.clusters)
-
-set.seed(2)
-km.out14=kmeans(sd.data, 14, nstart=20)
-km.out14$betweenss/km.out14$totss
-km.clusters14=km.out14$cluster
-table(km.clusters14,hc.clusters)
-
-set.seed(2)
-km.out15=kmeans(sd.data, 15, nstart=20)
-km.out15$betweenss/km.out15$totss
-km.clusters15=km.out15$cluster
-table(km.clusters15,hc.clusters)
-
-set.seed(2)
-km.out16=kmeans(sd.data, 16, nstart=20)
-km.out16$betweenss/km.out16$totss
-km.clusters16=km.out16$cluster
-table(km.clusters16,hc.clusters)
+for (i in 1:20) {
+  print(i)
+  set.seed(2)
+  km.out=kmeans(sd.data, i, nstart=20)
+  km.out$betweenss/km.out$totss
+  km.clusters=km.out$cluster
+  #print(table(km.clusters,hc.clusters))
+  print(table(nci.labs,km.clusters))
+}
 
 ################################################
 #
 # Task 2c Various number of clusters
 #
 ################################################
-N = 60
+# Silhouette plots
+library(cluster)
+par(mfrow=c(1,1))
+for (i in 2:25) {
+  set.seed(2)
+  grps <- kmeans(sd.data, centers = i)
+  plot(silhouette(grps$cluster, data.dist))
+}
+
+# Within groups sum of squares
+N = 30
 wcvg = array(0,dim=N)
 x = array(1:N,dim=N)
 for (i in 1:N) {
   print(i)
+  set.seed(2)
   wcvg[i] = sum(kmeans(sd.data, centers = i)$withinss)
 }
-plot(x, wcvg, type = "b", xlab = "Number of groups", ylab = "Within groups sum of squares", col=ifelse(x==10, "red", "black"), pch=ifelse(x==10, 19, 1))
+ellbow = 4
+plot(x, wcvg, type = "b", xlab = "Number of groups", ylab = "Within groups sum of squares", col=ifelse(x==ellbow, "red", "black"), pch=ifelse(x==ellbow, 19, 1))
